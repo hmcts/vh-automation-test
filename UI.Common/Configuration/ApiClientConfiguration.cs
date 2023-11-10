d343a80d1c6301b34581853c2e08cb0b7e3d68c9
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-11-10 11:04:00 +0000

    update

--- UI.Common/Configuration/ApiClientConfiguration.cs
+++ UI.Common/Configuration/ApiClientConfiguration.cs
@@ -6,16 +6,8 @@ public class ApiClientConfiguration
     public string TenantId { get;set; }
     public string ClientId { get;set; }
     public string ClientSecret { get;set; }
-    public ApiClientDetails BookingsApi { get;set; }
     public string BookingsApiUrl { get;set; }
     public string BookingsApiResourceId { get;set; }
-    public ApiClientDetails VideoApi { get;set; }
-    public string VideoApiApiUrl { get;set; }
-    public string VideoApiApiResourceId { get;set; }
-}
-
-public class ApiClientDetails
-{
-    public string Url { get;set; }
-    public string ResourceId { get;set; }
+    public string VideoApiUrl { get;set; }
+    public string VideoApiResourceId { get;set; }
 }
