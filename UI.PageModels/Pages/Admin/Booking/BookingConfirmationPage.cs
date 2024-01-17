13e9a91c3f49dce49cc19382c4d2bb22ffa4390d
Philip Debrah <phil.debrah@justice.gov.uk>
2024-01-17 14:04:48 +0000

    Update Webelements

--- UI.PageModels/Pages/Admin/Booking/BookingConfirmationPage.cs
+++ UI.PageModels/Pages/Admin/Booking/BookingConfirmationPage.cs
@@ -1,9 +1,18 @@
-﻿namespace UI.PageModels.Pages.Admin.Booking;
+﻿using OpenQA.Selenium.Support.UI;
+
+namespace UI.PageModels.Pages.Admin.Booking;
 
 public class BookingConfirmationPage : VhAdminWebPage
 {
     private readonly By _bookAnotherHearingBtn = By.Id("btnBookAnotherHearing");
     private readonly By _viewBookingLink = By.XPath("//a[text()='View this booking']");
+
+    private readonly By _viewThisBooking =
+        By.XPath("//*[@id='main-content']/app-booking-confirmation/div/div/div[1]/a");
+
+    private readonly By _editHearing = By.XPath("//button[@id='edit-button']");
+    private readonly By _addNewNOJOH = By.LinkText("Judicial Office Holder(s)");
+    private readonly By _nextParticipantPage = By.XPath("//*[@id='nextButtonToParticipants']");
 
     public BookingConfirmationPage(IWebDriver driver, int defaultWaitTime) : base(driver, defaultWaitTime)
     {
@@ -28,5 +37,28 @@ public class BookingConfirmationPage : VhAdminWebPage
     {
         ClickElement(_viewBookingLink);
         return new BookingDetailsPage(Driver, DefaultWaitTime);
+    }
+
+    public void viewThisBooking()
+    {
+        ClickElement(_viewThisBooking);
+    }
+
+    public void editHearing()
+    {
+        IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+        js.ExecuteScript("arguments[0].scrollIntoView(true);", 
+            Driver.FindElement(_editHearing));
+        ClickElement(_editHearing);
+    }
+
+    public void AddNewNoJoh()
+    {
+        ClickElement(_addNewNOJOH);
+    }
+
+    public void NextParticipantPage()
+    {
+        ClickElement(_nextParticipantPage);
     }
 }
