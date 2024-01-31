36225f46564d5c9adff0fb9ea90ae0c6cbcd2e5d
William Craig <mr.craig1991@gmail.com>
2024-01-31 13:47:12 +0000

    Method renaming

--- UI.PageModels/Pages/Admin/Booking/HearingDetailsPage.cs
+++ UI.PageModels/Pages/Admin/Booking/HearingDetailsPage.cs
@@ -16,6 +16,64 @@ public class HearingDetailsPage : VhAdminWebPage
     }
 
     /// <summary>
+    /// Go to the next page or the booking journey, the hearing schedule page
+    /// </summary>
+    /// <returns></returns>
+    public HearingSchedulePage GoToNextPage()
+    {
+        ClickElement(_nextButton);
+        return new HearingSchedulePage(Driver, DefaultWaitTime);
+    }
+
+    /// <summary>
+    /// Enter the details for a hearing and go to the summary page
+    /// </summary>
+    /// <param name="bookingDto">A DTO representing a booking</param>
+    /// <returns>the summary page</returns>
+    public SummaryPage BookAHearingJourney(BookingDto bookingDto, bool isV2)
+    {
+        if(isV2)
+            EnterHearingDetailsV2(bookingDto.CaseNumber, bookingDto.CaseName, bookingDto.CaseType);
+        else
+            EnterHearingDetails(bookingDto.CaseNumber, bookingDto.CaseName, bookingDto.CaseType, bookingDto.HearingType);
+        
+        var hearingSchedulePage = GoToNextPage();
+
+        hearingSchedulePage.EnterSingleDayHearingSchedule(
+            bookingDto.ScheduledDateTime, 
+            bookingDto.DurationHour,
+            bookingDto.DurationMinute,
+            bookingDto.VenueName, 
+            bookingDto.RoomName);
+
+        var assignJudgePage = hearingSchedulePage.GoToNextPage();
+
+        if (isV2)
+        {
+            assignJudgePage.AssignPresidingJudiciaryDetails(bookingDto.Judge.Username, bookingDto.Judge.DisplayName);
+            assignJudgePage.ClickSaveEJudgeButton();
+        }
+        else
+            assignJudgePage.EnterJudgeDetails(bookingDto.Judge.Username, bookingDto.Judge.DisplayName, bookingDto.Judge.Phone);
+
+        var addParticipantPage = assignJudgePage.GoToParticipantsPage(isV2);
+        
+        if(isV2)
+            addParticipantPage.AddExistingParticipantsV2(bookingDto.Participants);
+        else
+            addParticipantPage.AddExistingParticipants(bookingDto.Participants);
+        
+        var videoAccessPointsPage = addParticipantPage.GoToVideoAccessPointsPage();
+        videoAccessPointsPage.AddVideoAccessPoints(bookingDto.VideoAccessPoints);
+        
+        var otherInformationPage = videoAccessPointsPage.GoToOtherInformationPage();
+        otherInformationPage.TurnOffAudioRecording();
+        otherInformationPage.EnterOtherInformation(bookingDto.OtherInformation);
+
+        return otherInformationPage.GoToSummaryPage();
+    }
+    
+    /// <summary>
     /// Enter the case details, case type and hearing type
     /// </summary>
     /// <param name="caseNumber">the case number, typically the property to search against</param>
@@ -40,58 +98,5 @@ public class HearingDetailsPage : VhAdminWebPage
         WaitForDropdownListToPopulate(_caseType);
         SelectDropDownByText(_caseType, caseType);
         
-    }
-
-    /// <summary>
-    /// Go to the next page or the booking journey, the hearing schedule page
-    /// </summary>
-    /// <returns></returns>
-    public HearingSchedulePage GoToNextPage()
-    {
-        ClickElement(_nextButton);
-        return new HearingSchedulePage(Driver, DefaultWaitTime);
-    }
-
-    /// <summary>
-    /// Enter the details for a hearing and go to the summary page
-    /// </summary>
-    /// <param name="bookingDto">A DTO representing a booking</param>
-    /// <returns>the summary page</returns>
-    public SummaryPage EnterHearingDetails(BookingDto bookingDto, bool isV2)
-    {
-        if(isV2)
-            EnterHearingDetailsV2(bookingDto.CaseNumber, bookingDto.CaseName, bookingDto.CaseType);
-        else
-            EnterHearingDetails(bookingDto.CaseNumber, bookingDto.CaseName, bookingDto.CaseType, bookingDto.HearingType);
-        
-        var hearingSchedulePage = GoToNextPage();
-
-        hearingSchedulePage.EnterSingleDayHearingSchedule(bookingDto.ScheduledDateTime, bookingDto.DurationHour,
-            bookingDto.DurationMinute, bookingDto.VenueName, bookingDto.RoomName);
-
-        var assignJudgePage = hearingSchedulePage.GoToNextPage();
-
-        if (isV2)
-        {
-            assignJudgePage.AssignPresidingJudiciaryDetails(bookingDto.Judge.Username, bookingDto.Judge.DisplayName);
-            assignJudgePage.ClickSaveEJudgeButton();
-        }
-        else
-            assignJudgePage.EnterJudgeDetails(bookingDto.Judge.Username, bookingDto.Judge.DisplayName, bookingDto.Judge.Phone);
-
-        var addParticipantPage = assignJudgePage.GoToParticipantsPage(isV2);
-        
-        if(isV2)
-            addParticipantPage.AddExistingParticipantsV2(bookingDto.Participants);
-        else
-            addParticipantPage.AddExistingParticipants(bookingDto.Participants);
-        
-        var videoAccessPointsPage = addParticipantPage.GoToVideoAccessPointsPage();
-        videoAccessPointsPage.AddVideoAccessPoints(bookingDto.VideoAccessPoints);
-        var otherInformationPage = videoAccessPointsPage.GoToOtherInformationPage();
-        otherInformationPage.TurnOffAudioRecording();
-        otherInformationPage.EnterOtherInformation(bookingDto.OtherInformation);
-
-        return otherInformationPage.GoToSummaryPage();
     }
 }
