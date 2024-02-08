2b77dae34ab432c393e5774f5c9e70bdc0714b97
William Craig <mr.craig1991@gmail.com>
2024-02-08 13:45:16 +0000

    Conference details sometimes not ready once a hearing has been booked, so created a method to poll for instead

--- UI.NUnitVersion/Video/VideoWebUiTest.cs
+++ UI.NUnitVersion/Video/VideoWebUiTest.cs
@@ -1,9 +1,11 @@
+using System.Net;
 using UI.NUnitVersion.Models;
 using UI.PageModels.Pages.Video;
 using UI.PageModels.Pages.Video.Participant;
 using UI.PageModels.Pages.Video.QuickLink;
 using UI.PageModels.Pages.Video.Vho;
 using VideoApi.Client;
+using VideoApi.Contract.Responses;
 
 namespace UI.NUnitVersion.Video;
 
@@ -56,6 +58,38 @@ public abstract class VideoWebUiTest : CommonUiTest
             x.Driver.Terminate();
         });
         ParticipantDrivers.Clear();
+    }
+    
+    protected virtual async Task<ConferenceDetailsResponse> GetConference(Guid hearingId)
+    {
+        var pollCount = 0;
+        ConferenceDetailsResponse conferenceResponse;
+        do {
+            conferenceResponse = await PollForConferenceDetails(); 
+            pollCount++;
+        } while (conferenceResponse == null);
+
+        return conferenceResponse;
+        
+        async Task<ConferenceDetailsResponse> PollForConferenceDetails()
+        {
+            try
+            {
+                return await VideoApiClient.GetConferenceByHearingRefIdAsync(hearingId, true);
+            }
+            catch (VideoApiException e)
+            {
+                if(pollCount >= 3) 
+                    throw new NotFoundException($"Conference not found for hearing {hearingId} after 3 attempts");
+                
+                if (e.StatusCode == (int) HttpStatusCode.NotFound)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                    return null;
+                }
+                throw;
+            }
+        }
     }
     
     protected virtual Task CleanUp()
