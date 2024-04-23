6388b0c2ce40385303498da0bffe9b5209e90d34
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-04-23 16:42:13 +0100

    revert WaitForElementToBeInvisible

--- UI.PageModels/Pages/VhPage.cs
+++ UI.PageModels/Pages/VhPage.cs
@@ -231,29 +231,8 @@ public abstract class VhPage
 
     protected void WaitForElementToBeInvisible(By locator, int timeOut)
     {
-        var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeOut))
-        {
-            PollingInterval = TimeSpan.FromMilliseconds(500), // Check every half second
-        };
-
-        wait.IgnoreExceptionTypes(typeof(ElementNotVisibleException));
-
-        wait.Until(driver =>
-        {
-            try
-            {
-                var element = driver.FindElement(locator);
-                return !element.Displayed; // Return true (stop waiting) when the element is not displayed
-            }
-            catch (NoSuchElementException)
-            {
-                return true; // Element is not found, so it's "invisible"
-            }
-            catch (StaleElementReferenceException)
-            {
-                return false; // Element is stale, continue waiting
-            }
-        });
+        new WebDriverWait(Driver, TimeSpan.FromSeconds(timeOut))
+            .Until(ExpectedConditions.InvisibilityOfElementLocated(locator));
     }
 
     protected string GetLocaleDate(DateTime date)
