fa63b752a3100cac14060319f7a94666375d6d70
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2024-11-26 10:53:54 +0000

    config changes for chrome

--- UI.AutomationTests/Drivers/LocalChromeVhDriver.cs
+++ UI.AutomationTests/Drivers/LocalChromeVhDriver.cs
@@ -21,6 +21,14 @@ public class LocalChromeVhDriver : IVhDriver
         chromeOptions.AddArgument("no-sandbox");
         chromeOptions.AddArguments("--use-fake-ui-for-media-stream");
         chromeOptions.AddArguments("--use-fake-device-for-media-stream");
+
+        if (Environment.GetEnvironmentVariable("TF_BUILD") != null)
+        {
+            chromeOptions.AddArgument("--disable-dev-shm-usage"); // Overcome limited resource problems
+            chromeOptions.AddArgument("--headless"); // Run in headless mode if needed
+            chromeOptions.AddArgument("--disable-gpu"); // Applicable to Windows OS only
+            chromeOptions.AddArgument("--remote-debugging-port=9222"); // Debugging port
+        }
         _driver = new ChromeDriver(cService, chromeOptions);
     }
 
