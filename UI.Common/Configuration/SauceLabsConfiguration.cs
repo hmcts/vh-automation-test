5df37e34d17a90c6d37b0b3731dd47fb7bf9bd38
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2024-12-06 15:39:04 +0000

    remove unused app settings

--- UI.Common/Configuration/SauceLabsConfiguration.cs
+++ UI.Common/Configuration/SauceLabsConfiguration.cs
@@ -4,16 +4,10 @@ public class SauceLabsConfiguration
 {
     public string MacScreenResolution => "2360x1770";
     public string WindowsScreenResolution => "2560x1600";
-    public string PlatformName { get; set; }
-    public string BrowserName { get; set; }
-    public string DeviceName { get; set; }
-    public string PlatformVersion { get; set; }
-    public string AppiumVersion { get; set; }
     public string SauceUsername { get; set; }
     public string SauceAccessKey { get; set; }
     public string SauceUrl { get; set; }
     public string SecureSauceUrl { get; set; }
-    public string Orientation { get; set; }
 
     // SauceLabOptions
     public int MaxDurationInSeconds => 60 * 10;
--- UI.Common/Configuration/SystemConfigSettings.cs
+++ UI.Common/Configuration/SystemConfigSettings.cs
@@ -2,13 +2,9 @@ namespace UI.Common.Configuration;
 
 public class SystemConfigSettings
 {
-    public string TestResultsDirectory { get; set; }
     public string ReportLocation { get; set; }
-    public string ImageLocation { get; set; }
     public bool RunOnSauceLabs { get; set; }
     public bool RunHeadlessBrowser { get; set; }
     public bool EnableAccessibilityCheck { get; set; }
-    public string AccessibilityReportFilePath { get; set; }
-    public string AccessibilityHtmlReportFilePath { get; set; }
     public SauceLabsConfiguration SauceLabsConfiguration { get; set; }
 }
