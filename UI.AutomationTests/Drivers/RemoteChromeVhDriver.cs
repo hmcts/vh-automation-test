38459575e132e43efbeac9c830f8fd60dbf2807e
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2025-01-10 16:32:03 +0000

    Fix ui stream (#269)

--- UI.AutomationTests/Drivers/RemoteChromeVhDriver.cs
+++ UI.AutomationTests/Drivers/RemoteChromeVhDriver.cs
@@ -9,7 +9,7 @@ public class RemoteChromeVhDriver : IVhDriver
     private RemoteWebDriver _driver;
 
     public RemoteChromeVhDriver(string platform = "Windows 11", string browserVersion = "latest",
-        string username = null)
+        string username = null, string videoFileName = null)
     {
         var envConfigSettings = ConfigRootBuilder.EnvConfigInstance();
         var chromeOptions = new ChromeOptions
@@ -20,8 +20,7 @@ public class RemoteChromeVhDriver : IVhDriver
 
         chromeOptions.AddArguments("start-maximized");
         chromeOptions.AddArgument("no-sandbox");
-        chromeOptions.AddArguments("--use-fake-ui-for-media-stream");
-        chromeOptions.AddArguments("--use-fake-device-for-media-stream");
+        chromeOptions.AddArgument("--use-fake-ui-for-media-stream");
         
         // this is the name for a build in SauceLabs
         var buildName = Environment.GetEnvironmentVariable("TF_BUILD") == null
