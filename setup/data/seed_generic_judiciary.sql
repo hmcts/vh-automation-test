f7cbf763ab6f8af1b88e93cf901c6e89a247cd7f
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2025-02-11 16:13:24 +0000

    update seed data

--- setup/data/seed_generic_judiciary.sql
+++ setup/data/seed_generic_judiciary.sql
@@ -1,4 +1,4 @@
-SET XACT_ABORT ON
+SET XACT_ABORT ON;
 GO;
 BEGIN TRANSACTION;
 
@@ -6,13 +6,46 @@ BEGIN TRANSACTION;
 -- VH-GENERIC-ACCOUNT-0
 
 MERGE INTO JudiciaryPerson AS target
-USING (VALUES 
-    ('3DBB214B-9080-43B4-9EBA-A8877989031D', '3DBB214B-9080-43B4-9EBA-A8877989031D', 'VH-GENERIC-ACCOUNT-0', 'Auto', 'Judge', 'Automation Test Judge', 'auto_aw.judge_02@hearings.reform.hmcts.net', '2024-01-05 12:21:39.0000000', '2024-01-05 12:22:15.0000000', false, false, null, null, null, true, false)
+USING (VALUES
+    ('3DBB214B-9080-43B4-9EBA-A8877989031D',
+     '3DBB214B-9080-43B4-9EBA-A8877989031D',
+     'VH-GENERIC-ACCOUNT-0',
+     null,
+     'Judge',
+     'Judge',
+     'Automation Test Judge',
+     'auto_aw.judge_02@hearings.reform.hmcts.net',
+     '2024-01-05 12:21:39.0000000',
+     '2024-01-05 12:22:15.0000000',
+     0,
+     0,
+     null,
+     null,
+     1,
+     0,
+     null),
+    ('63FE82B7-C9BD-4BE2-94F4-8DF4C0A18BAF',
+     '63fe82b7-c9bd-4be2-94f4-8df4c0a18baf',
+     'VH-GENERIC-ACCOUNT-02',
+     'Mr',
+     'Auto',
+     'Panel',
+     'Automation Test Panel Member',
+     'auto_aw.panelmember_01@hearings.reform.hmcts.net',
+     '2024-01-05 12:21:39.0000000',
+     '2024-01-05 12:22:15.0000000',
+     0,
+     0,
+     null,
+     '1234567890',
+     1,
+     0,
+     null)
 ) AS source (Id, ExternalRefId, PersonalCode, Title, KnownAs, Surname, Fullname, Email, CreatedDate, UpdatedDate, HasLeft, Leaver, LeftOn, WorkPhone, IsGeneric, Deleted, DeletedOn)
 ON (target.PersonalCode = source.PersonalCode)
 
 WHEN MATCHED THEN
-    UPDATE SET 
+    UPDATE SET
         target.ExternalRefId = source.ExternalRefId,
         target.Title = source.Title,
         target.KnownAs = source.KnownAs,
@@ -34,7 +67,7 @@ WHEN NOT MATCHED THEN
 
 SELECT *
 FROM dbo.JudiciaryPerson
-WHERE PersonalCode = 'VH-GENERIC-ACCOUNT-0';
+WHERE PersonalCode IN ('VH-GENERIC-ACCOUNT-0', 'VH-GENERIC-ACCOUNT-02');
 
 COMMIT TRANSACTION;
 SET XACT_ABORT OFF;
