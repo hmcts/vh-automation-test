7c3ec1766ba420d3de3dc09fc0633e404ccf246a
Oliver Scott <oliver.scott@justice.gov.uk>
2025-03-03 15:00:37 +0000

    Restore removed test

--- UI.AutomationTests/Admin/Booking/EditBookingTest.cs
+++ UI.AutomationTests/Admin/Booking/EditBookingTest.cs
@@ -1,6 +1,5 @@
 
 using UI.Common.Utilities;
-using UI.PageModels.Pages.Admin.Booking;
 
 namespace UI.AutomationTests.Admin.Booking;
 
@@ -36,11 +35,76 @@ public class EditBookingTest : HearingTest
         bookingDetailPage.ValidateDetailsPage(hearingDto);
         Assert.Pass();
     }
+    
+    [Test]
+    [FeatureToggleSetting(FeatureToggle.InterpreterEnhancementsToggleKey, true)]
+    [Category("admin")]
+    public void should_update_booking_with_interpreter_languages()
+    {
+        var date = DateTime.Today.AddDays(1).AddHours(10).AddMinutes(30);
+        var interpreterLanguage = new InterpreterLanguageDto("Spanish", InterpreterType.Verbal);
+        var bookingDto = HearingTestData.CreateHearingDtoWithInterpreterLanguages(HearingTestData.JudgePersonalCode,
+            judgeUsername: HearingTestData.JudgeUsername, scheduledDateTime: date, interpreterLanguage);
+        bookingDto.CaseNumber = $"Automation Test Hearing - BookAHearing {Guid.NewGuid():N}";
+        var bookingDetailsPage = BookHearingAndGoToDetailsPage(bookingDto);
+
+        var summaryPage = bookingDetailsPage.UpdateSchedule(bookingDto.ScheduledDateTime, bookingDto.DurationHour,
+            bookingDto.DurationMinute);
+
+        var newInterpreterLanguage = new InterpreterLanguageDto("British Sign Language (BSL)", InterpreterType.Sign);
+
+        // Assign a new judge
+        var alternativeJudge = new BookingJudiciaryParticipantDto(HearingTestData.JudgePersonalCode, HearingTestData.AltJudgeUsername,
+            "Auto Judge 2", "")
+        {
+            InterpreterLanguage = newInterpreterLanguage
+        };
+        var assignJudgePage = summaryPage.ChangeJudgeV2();
+        assignJudgePage.EnterJudgeDetails(alternativeJudge);
+        bookingDto.Judge = alternativeJudge;
+        summaryPage = assignJudgePage.GotToNextPageOnEdit();
+
+        // Update the participants
+        var participantsPage = summaryPage.ChangeParticipants();
+        foreach (var participant in bookingDto.Participants.Where(p => p.Role != GenericTestRole.Representative)
+                     .ToList()) // There is a bug updating representatives, so skip them for now
+        {
+            participant.InterpreterLanguage = newInterpreterLanguage;
+            Thread.Sleep(5000); // Allow time for the edit link to be clickable
+            participantsPage.UpdateParticipant(participant.FullName, participant.DisplayName, newInterpreterLanguage);
+        }
+
+        var videoAccessPointsPage = participantsPage.GoToVideoAccessPointsPage();
+
+        // Update the endpoints
+        var sortedEndpoints = bookingDto.VideoAccessPoints.OrderBy(x => x.DisplayName).ToList();
+        foreach (var endpoint in sortedEndpoints)
+        {
+            endpoint.InterpreterLanguage = newInterpreterLanguage;
+        }
+
+        const int endpointIndexToUpdate = 0;
+        var endpointToUpdate = sortedEndpoints[endpointIndexToUpdate];
+        videoAccessPointsPage.UpdateVideoAccessPoint(endpointIndexToUpdate, "None", newInterpreterLanguage);
+        endpointToUpdate.DefenceAdvocateDisplayName = "";
+        var otherInformationPage = videoAccessPointsPage.GoToSpecialMeasuresPage().GoToOtherInformationPage();
+
+        summaryPage = otherInformationPage.GoToSummaryPage();
+
+        summaryPage.ValidateSummaryPage(bookingDto);
+        var confirmationPage = summaryPage.ClickBookButton();
+
+        confirmationPage.IsBookingSuccessful().Should().BeTrue();
+        var bookingDetailPage = confirmationPage.ClickViewBookingLink();
+        bookingDetailPage.ValidateDetailsPage(bookingDto);
+
+        Assert.Pass();
+    }
 
     [Test]
     [FeatureToggleSetting(FeatureToggle.SpecialMeasuresKey, true)]
     [Category("admin")]
-    public async Task EditAHearingWithScreening()
+    public async Task should_update_booking_with_screening()
     {
         // Book a hearing with screening participants
         var hearingScheduledDateAndTime = DateUtil
