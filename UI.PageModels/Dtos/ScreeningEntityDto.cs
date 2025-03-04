2d001b4040107c9c7f8d158ae49f5651361938a7
Oliver Scott <oliver.scott@justice.gov.uk>
2025-03-04 09:49:39 +0000

    Change to record

--- UI.PageModels/Dtos/ScreeningEntityDto.cs
+++ UI.PageModels/Dtos/ScreeningEntityDto.cs
@@ -1,11 +1,3 @@
 namespace UI.PageModels.Dtos;
 
-public class ScreeningEntityDto
-{
-    public string DisplayName { get; set; }
-
-    public ScreeningEntityDto(string displayName)
-    {
-        DisplayName = displayName;
-    }
-}
+public record ScreeningEntityDto(string DisplayName);
